f67498bd225126cbc10fdcaaa6dc6a1d29ddf103
1XDeNX1 <57237634+1XDeNX1@users.noreply.github.com>
2020-09-16 18:05:39 +0300

    Задание выполнил

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите A: ");
+            string A = Console.ReadLine();
+            Console.Write("Введите B: ");
+            string B = Console.ReadLine();
+            Console.Write("Введите C: ");
+            string C = Console.ReadLine();
+            string t = A;
+            A = B;
+            B = C;
+            C = t;
+            Console.WriteLine($"Значение A: {A}");
+            Console.WriteLine($"Значение B: {B}");
+            Console.WriteLine($"Значение C: {C}");
+            Console.ReadKey();
+        }
+    }
+}
